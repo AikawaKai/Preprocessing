6681eefbf720a2bbd14738313c468eab20f741d9
kai <marco.odore@gmail.com>
2016-10-03 18:56:13 +0200

    updated

--- main.cpp
+++ main.cpp
@@ -23,7 +23,9 @@ int main(){
 	x3.min = 1;
 	x3.max = 5;
 	y4.min = 2;
-	y4.max = std::numeric_limits<int>::max();
+	y4.max = 4;
+	y5.max = 0;
+	y5.max = std::numeric_limits<int>::max();
 	
 	Variable equ1 []  = {x1, x2, x3, y4, y5};
 	int coeffEqu1 [] = {4, -3, -2, 1, 2};
