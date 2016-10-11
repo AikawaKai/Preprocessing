cda577369947fab7ff00b2ea7a2b521dc1f1aab9
kai <marco.odore@gmail.com>
2016-10-11 15:59:30 +0200

    updated

--- main.cpp
+++ main.cpp
@@ -41,9 +41,9 @@ int main(){
 	int b2 = -9;
 	int numrow = 2;
 	int numcol = 5;
-	int coeffEqu [numrow][numcol+1] = {{4, -3, -2, 1, 2, b1}, {-3, 2, -1, 2, 3, b2}};*/
+	int coeffEqu [numrow][numcol+1] = {{4, -3, -2, 1, 2, b1}, {-3, 2, -1, 2, 3, b2}};
 	
-	//*SECOND EXAMPLE: BINARY VARIABLE
+	/*SECOND EXAMPLE: BINARY VARIABLE
 	binVar *y1 = new binVar();
 	binVar *y2 = new binVar();
 	binVar *y3 = new binVar();
@@ -59,14 +59,43 @@ int main(){
 	int b2 = -5;
 	int numrow = 2;
 	int numcol = 4;
-	int coeffEqu [2][numcol+1]={{8, 11, -9, 4, b1}, {1, -4, -6, 1, b2}};
+	int coeffEqu [numrow][numcol+1]={{8, 11, -9, 4, b1}, {1, -4, -6, 1, b2}};
 	
 	cout<<"[Before update]\n";
 	print_constraints(&cond, (int *)coeffEqu, numrow, numcol);
 	
 	cout<<"\n[After update]\n";
 	boundsPreprocess(&cond, (int *)coeffEqu, numrow, numcol);
-	print_constraints(&cond, (int *)coeffEqu, numrow, numcol);
+	print_constraints(&cond, (int *)coeffEqu, numrow, numcol);*/
+	
+	//*Third Example
+	intVar *x1 = new intVar();
+	intVar *x2 = new intVar();
+	intVar *x3 = new intVar();
+	intVar *x4 = new intVar();
+	x1->name = "X1";
+	x2->name = "X2";
+	x3->name = "X3";
+	x4->name = "X4";
+	x1->min = 0;
+	x1->max = 2;
+	x2->min = 0;
+	x2->max = 1;
+	x3->min = 1;
+	x3->max = 2;
+	x4->min = 2;
+	x4->max = 3;
+	int numcol = 4;
+	int numrow = 3;
+	int b1 = -6;
+	int b2 = 4;
+	int b3 = 0;
+	int coeffEqu [numrow][numcol+1]={{1, 1, 1, -2, b1}, {-1, -3, 2, -1, b2}, {-1, 1, 0, 1, b3}};
+	vector<Variable*> cond;
+	Variable *lista[] = {x1, x2, x3, x4};
+	cond.insert(cond.begin(),lista, lista+4);
+	print_constraints(&cond, (int*)coeffEqu, numrow, numcol);
+	
 }
 
 
--- preprocess.h
+++ preprocess.h
@@ -2,4 +2,4 @@
 #include <vector>    
 
 void boundsPreprocess(std::vector<Variable*> *cond, int *coeffEqu, int numrow, int numcol);
-bool update(std::vector<Variable*> *cond, int numvar, int coeffEqu, int row, int pos);
+bool update(std::vector<Variable*> *cond, int numcol, int coeffEqu, int numrow, int pos);
