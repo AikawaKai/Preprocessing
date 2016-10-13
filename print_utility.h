58d29d8dee249e6392376ab6443904c9235e5e99
kai <marco.odore@gmail.com>
2016-10-13 16:25:19 +0200

    fixed namespace problem

--- print_utility.h
+++ print_utility.h
@@ -4,4 +4,4 @@
 #include <vector>
 
 void printConstraints(std::vector<Variable*> *cond, float *coeffEqu, int numrow, int numcol);
-bool checkRow(vector<Variable*> *cond, float *coeffEqu, int i, int numcol);
+bool checkRow(std::vector<Variable*> *cond, float *coeffEqu, int i, int numcol);
