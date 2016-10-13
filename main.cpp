45b217058f684cf33fe3cdb47246df528d0814fc
kai <marco.odore@gmail.com>
2016-10-13 15:53:23 +0200

    updated

--- main.cpp
+++ main.cpp
@@ -12,6 +12,16 @@ int main(){
 	floatVar *x2 = new floatVar("x2", 0, 1);
 	floatVar *x3 = new floatVar("x3", 1, 2);
 	floatVar *x4 = new floatVar("x4", 2, 3);
+	Variable *list[4] = {x1, x2, x3, x4};
+	vector<Variable*>cond;
+	cond.insert(cond.begin(),list, list+4); 
+	float b1 = -6;
+	float b2 = 4;
+	float b3 = 0;
+	int numrow = 3;
+	int numcol = 4;
+	float coeffEqu[numrow][numcol+1] = {{1, 1, 1, -2, b1},{-1, -3, 2, -1, b2},{-1, 1, 0, 1, b3}};
+	printConstraints(&cond, (float*)coeffEqu, numrow, numcol);
 }
 
 
