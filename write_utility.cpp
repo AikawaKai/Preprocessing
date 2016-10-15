ff290aca660dd153fb9d89bc3727ac33cb2a3aa6
unknown <marco.odore@studenti.unimi.it>
2016-10-15 16:56:31 +0200

    udpated

--- write_utility.cpp
+++ write_utility.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
 #include "type.h"
 
 void writeDat(std::vector<Variable*> *cond, float *coeffEqu, int numrow, int numcol, int num_x, int num_y, int num_z)
 {
+	std::ofstream outfile;
+	outfile.open("./afile.dat");
+	outfile<<"ciao"<<std::endl;
+	outfile.close();
 	
 }
