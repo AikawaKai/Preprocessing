1258577944ffa00a88194a4c6ae4a544f5c053a4
unknown <marco.odore@studenti.unimi.it>
2016-10-15 16:24:18 +0200

    added stub function writeDat

---
+++ write_utility.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include "type.h"
+
+void writeDat(std::vector<Variable*> *cond, float *coeffEqu, int numcol, int numrow, int num_x, int num_y, int num_z)
+{
+	
+}
---
+++ write_utility.h
@@ -0,0 +1,6 @@
+#include <string>
+#include <iostream>
+#include <math.h>
+#include <vector>
+
+void writeDat(vector<Variable*> *cond, float *coeffEqu, int numcol, int numrow, int num_x, int num_y, int num_z)
